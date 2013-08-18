e0ecc5ee907d59020c69436a64588b7ca2260c9e
hunglinHsu <homling@gmail.com>
2013-08-18 10:48:39 +0800

    initial commit of rails with gitignore refined

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails2::Application.config.secret_token = '6e1863676ee2126a38f6f5026d1ffff88d8a92959b0b36da805e7ce31dd088ce394dfed77141e5ca3cb434bed33c5f85067c2d1d65c81101361488ddedd18985'
